c3623241f510fd5df9a17bcfef6f84b2f9d525d1
Rishabh28111995 <109350653+Rishabh28111995@users.noreply.github.com>
2023-04-02 14:47:02 +0530

    Add files via upload
    calculate the power of 2^n

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace PowerOfN;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the value of N: ");
+            int N = int.Parse(Console.ReadLine());
+        for (int i = 0; i < N; i++)
+        {
+            Console.WriteLine("2^{0} = {1}", i, Math.Pow(2, i));
+        }
+    }
+}
