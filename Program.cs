75e5eb97a48b659bbd3fdf6e88f69586a634f0ab
Rishabh28111995 <109350653+Rishabh28111995@users.noreply.github.com>
2023-04-09 11:46:16 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Alphabet;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the alphabet: ");
+        char ch = char.Parse(Console.ReadLine());
+
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+        {
+            Console.WriteLine("Alphabet is vowel.");
+        }
+        else
+        {
+            Console.WriteLine("Alphabet is consonant.");
+        }
+
+
+    }
+}
+
