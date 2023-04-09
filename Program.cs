58f2d2c172ab6c2d367d428cc6cc723274e6909d
Rishabh28111995 <109350653+Rishabh28111995@users.noreply.github.com>
2023-04-09 11:50:03 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace LeapYear;
+
+class LeapYear
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the year: ");
+        int year = int.Parse(Console.ReadLine());
+
+        if((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+        {
+            Console.WriteLine(year + " is a Leap year");
+        }
+        else
+        {
+            Console.WriteLine(year + " is not a Leap year) ");
+        }
+        
+    }
+}
