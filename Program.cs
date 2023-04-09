d24a9092abc940f85e5b58abe9e3f3c38f436cb8
Rishabh28111995 <109350653+Rishabh28111995@users.noreply.github.com>
2023-04-09 11:51:37 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace LargestNumber;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num1, num2, num3;
+        Console.WriteLine("Enter the number: ");
+
+        num1 = int.Parse(Console.ReadLine());
+        num2 = int.Parse(Console.ReadLine());
+        num3 = int.Parse(Console.ReadLine());
+
+        if(num1 > num2 && num2 > num3)
+        {
+            Console.WriteLine(num1 + " is largest");
+        }
+        else if(num2 > num3 && num3 > num1)
+        {
+            Console.WriteLine(num2 + " is largest");
+        }
+        else
+        {
+            Console.WriteLine(num3 + " is largest");
+        }
+    }
+}
+
