646f6e22d2c595b9dfe0d2dd93ec3c08ac90e3a8
Rishabh28111995 <109350653+Rishabh28111995@users.noreply.github.com>
2023-04-09 11:54:00 +0530

    Add files via upload

---
+++ EvenOrOdd.cs
@@ -0,0 +1,21 @@
+﻿namespace EvenOrOdd;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int N;
+        Console.WriteLine("Enter the number: ");
+         N = int.Parse(Console.ReadLine());
+
+        if(N % 2 == 0)
+        {
+            Console.WriteLine("Number is Even");
+
+        }
+        else
+        {
+            Console.WriteLine("Number is odd");
+        }
+    }
+}
+
