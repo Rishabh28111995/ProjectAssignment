9cd96ad432da2d1488333aae5f239d9ef2f84cb0
Rishabh28111995 <109350653+Rishabh28111995@users.noreply.github.com>
2023-04-09 11:56:49 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace QuotientRemainder;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int dividend;
+        int divisor;
+        int quotient;
+        int remainder;
+
+        Console.WriteLine("Enter the value of dividend: ");
+        dividend = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the value of divisor: ");
+        divisor = int.Parse(Console.ReadLine());
+
+        quotient = dividend / divisor;
+        remainder = dividend % divisor;
+
+        Console.WriteLine("Quotient is: " + quotient);
+        Console.WriteLine("Remainder is: " + remainder);
+    }
+}
+
